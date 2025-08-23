7bc6032a95bee2367238ce96ab53c591c62ce6f3
Nico Pascual <pascualnicolass@gmail.com>
2025-08-22 22:42:00 -0300

    first commit

---
+++ client/src/routes/login.tsx
@@ -0,0 +1,78 @@
+import { createFileRoute } from '@tanstack/react-router'
+import {z} from "zod";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {useAppForm} from "@/hooks/demo.form.ts";
+
+const schema = z.object({
+    email: z.string().min(1, 'Title is required'),
+    password: z.string().min(1, 'Description is required'),
+})
+
+export const Route = createFileRoute('/login')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+    const form = useAppForm({
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+        validators: {
+            onBlur: schema,
+        },
+        onSubmit: ({ value }) => {
+            console.log(value)
+            // Show success message
+            alert('Form submitted successfully!')
+        },
+    })
+
+  return  <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+          <Card>
+              <CardHeader>
+                  <CardTitle>Login to your account</CardTitle>
+                  <CardDescription>
+                      Enter your email below to login to your account
+                  </CardDescription>
+              </CardHeader>
+              <CardContent>
+                  <form onSubmit={(e) => {
+                      e.preventDefault()
+                      e.stopPropagation()
+                      form.handleSubmit()
+                  }}>
+                      <div className="flex flex-col gap-6">
+                          <div className="grid gap-3">
+                              <form.AppField name="email">
+                                  {(field) => <field.TextField label="Email" />}
+                              </form.AppField>
+                          </div>
+                          <div className="grid gap-3">
+                              <form.AppField name="password">
+                                  {(field) => <field.TextField label="Password" />}
+                              </form.AppField>
+                          </div>
+                          <form.AppForm>
+                              <form.SubscribeButton label="Submit" />
+                          </form.AppForm>
+                      </div>
+                      <div className="mt-4 text-center text-sm">
+                          Don&apos;t have an account?{" "}
+                          <a href="#" className="underline underline-offset-4">
+                              Sign up
+                          </a>
+                      </div>
+                  </form>
+              </CardContent>
+          </Card>
+      </div>
+  </div>
+}
